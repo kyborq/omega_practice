88824247da0366d6616d8229ae31393bbb8db68e
Konstantin <k.podyganov@mail.ru>
2023-06-29 02:22:20 +0300

    continue

--- lib/src/screens/splash_screen.dart
+++ lib/src/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:omega_practice/src/router/app_pages.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -6,7 +7,9 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(AppPages.splash.toTitle),
+      ),
       body: Column(),
     );
   }
